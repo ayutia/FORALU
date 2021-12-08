f32502977282a32391afc84fd5a397c4d1880968
Sebastian Urrutia Torres <sebak93@gmail.com>
2021-12-08 00:05:58 -0300

    Primeras clases

---
+++ FORALU/Estudiante.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FORALU
+{
+    class Estudiante
+    {
+        private string _nombre;
+        private string _apellidoPaterno;
+        private string _apellidoMaterno;
+        private int _edad;
+
+        public Estudiante(string nombre, string apellidoPaterno, string ApellidoMaterno, int edad)
+        {
+            _nombre= nombre;
+            _apellidoPaterno = apellidoPaterno;
+            _apellidoMaterno = ApellidoMaterno;
+            _edad = edad;
+        }
+
+    }
+}
